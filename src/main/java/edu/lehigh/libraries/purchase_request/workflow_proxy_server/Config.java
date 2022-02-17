3f94cbe93bb6574b857849b1b970101f218a32d8
Maccabee Levine <msl321@lehigh.edu>
2022-02-17 14:33:46 -0500

    Use lombok to reduce boilerplate code

--- src/main/java/edu/lehigh/libraries/purchase_request/workflow_proxy_server/Config.java
+++ src/main/java/edu/lehigh/libraries/purchase_request/workflow_proxy_server/Config.java
@@ -3,29 +3,23 @@ package edu.lehigh.libraries.purchase_request.workflow_proxy_server;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Configuration
 @ConfigurationProperties(prefix="workflow")
+@Getter @Setter
 public class Config {
     
     private Jira jira;
 
-    public Jira getJira() { return jira; }
-    public void setJira(Jira jira) { this.jira = jira; }
-
+    @Getter @Setter
     public static class Jira {
 
-        private String url;
+        private String url;        
         private String username;
         private String token;
-    
-        public String getUrl() { return url; }
-        public void setUrl(String url) { this.url = url; }
 
-        public String getUsername() { return username; }   
-        public void setUsername(String username) { this.username = username; }
-
-        public String getToken() { return token; }
-        public void setToken(String token) { this.token = token; }
     }
 }
 
