66b99ac56417d3e520c4ebbfbd81fe655c3d344b
Maccabee Levine <msl321@lehigh.edu>
2022-02-23 13:48:41 -0500

    Generate configuration metadata with descriptions

--- src/main/java/edu/lehigh/libraries/purchase_request/workflow_proxy_server/Config.java
+++ src/main/java/edu/lehigh/libraries/purchase_request/workflow_proxy_server/Config.java
@@ -14,18 +14,76 @@ import lombok.Setter;
 public class Config {
     
     private Jira jira;
+    private Database db;
 
     @Getter @Setter
     public static class Jira {
 
+        /**
+         * URL to Jira, i.e. https://mydomain.atlassian.net/
+         */
         private String url;        
+
+        /**
+         * Jira username
+         */
         private String username;
+
+        /**
+         * Jira API token.  
+         * 
+         * See https://support.atlassian.com/atlassian-account/docs/manage-api-tokens-for-your-atlassian-account/
+         */
         private String token;
+
+        /**
+         * ID of the Jira issue type that should be created for purchase requests.
+         * 
+         * A list of types with IDs can be retrieved with {{jira.url}}/issuetype
+         */
         private Long issueTypeId;
+
+        /**
+         * ID of the Jira status that indicates a purchase request has been approved.
+         * 
+         * A list of statuses with IDs can be retrieved with {{jira.url}}/status
+         */
         private Long approvedStatusId;
+
+        /**
+         * ID of the Jira custom field representing the item's contributor (author etc.).
+         * 
+         * A list of fields with IDs can be retrieved with {{jira.url}}/field
+         */
         private String contributorFieldId;
 
     }
+
+    @Getter @Setter
+    /**
+     * Connection properties for database used for workflow proxy metadata.
+     * 
+     * This database is not used for the actual workflow items backed by a WorkflowService.
+     */
+    public static class Database {
+
+        /**
+         * Database hostname
+         */
+        private String host;
+
+        /**
+         * Database username
+         */
+        private String username;
+
+        /**
+         * Database password
+         */
+        private String password;
+
+    }
+
 }
 
 
