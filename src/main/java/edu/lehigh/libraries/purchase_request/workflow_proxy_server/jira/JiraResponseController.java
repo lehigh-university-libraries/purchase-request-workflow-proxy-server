0e465844842dd06741ade15a527e34632ba740c9
Maccabee Levine <msl321@lehigh.edu>
2022-02-24 15:47:59 -0500

    Ignore RequestBody, can't trust it since the message is not authenticated

--- src/main/java/edu/lehigh/libraries/purchase_request/workflow_proxy_server/jira/JiraResponseController.java
+++ src/main/java/edu/lehigh/libraries/purchase_request/workflow_proxy_server/jira/JiraResponseController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
@@ -17,8 +16,8 @@ public class JiraResponseController {
     private JiraWorkflowService service;
 
     @PostMapping("/purchase-requests/status/{issueKey}")
-    void statusChanged(@PathVariable String issueKey, @RequestBody String body) {
-        log.debug("Received message from Jira: " + body);
+    void statusChanged(@PathVariable String issueKey) {
+        log.debug("Received message from Jira about key: " + issueKey);
         confirmAndReportStatus(issueKey);
     }
 
