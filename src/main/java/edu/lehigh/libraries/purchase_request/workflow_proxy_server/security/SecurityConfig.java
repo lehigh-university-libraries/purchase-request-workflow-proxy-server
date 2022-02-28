e0df459a8044be3702068bf782df0f0660c5fc33
Maccabee Levine <msl321@lehigh.edu>
2022-02-28 13:08:29 -0500

    Allow all CORS origins

--- src/main/java/edu/lehigh/libraries/purchase_request/workflow_proxy_server/security/SecurityConfig.java
+++ src/main/java/edu/lehigh/libraries/purchase_request/workflow_proxy_server/security/SecurityConfig.java
@@ -17,6 +17,8 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AnyRequestMatcher;
 import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
@@ -70,6 +72,16 @@ public class SecurityConfig {
         authProvider.setUserDetailsService(userDetailsService());
         authProvider.setPasswordEncoder(passwordEncoder());
         return authProvider;
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
     }
 
 }
