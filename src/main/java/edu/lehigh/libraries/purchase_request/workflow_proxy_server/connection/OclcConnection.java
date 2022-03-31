931ca7f93ddaea6833a1c014a08bfa32d436653a
Maccabee Levine <msl321@lehigh.edu>
2022-03-31 14:23:19 -0400

    Renew token before expiration

--- src/main/java/edu/lehigh/libraries/purchase_request/workflow_proxy_server/connection/OclcConnection.java
+++ src/main/java/edu/lehigh/libraries/purchase_request/workflow_proxy_server/connection/OclcConnection.java
@@ -1,5 +1,7 @@
 package edu.lehigh.libraries.purchase_request.workflow_proxy_server.connection;
 
+import java.time.Instant;
+
 import com.github.scribejava.core.builder.ServiceBuilder;
 import com.github.scribejava.core.builder.api.DefaultApi20;
 import com.github.scribejava.core.model.OAuth2AccessToken;
@@ -17,9 +19,11 @@ import lombok.extern.slf4j.Slf4j;
 public class OclcConnection {
 
     public static final String WORLDCAT_BASE_URL = "https://americas.discovery.api.oclc.org/worldcat/search/v2";
+    private static final long TOKEN_BUFFER_SECONDS = 30;
 
     private final Config config;
     private OAuth2AccessToken token;
+    private long tokenExpiration;
     private OAuth20Service oclcService;
 
     public OclcConnection(Config config, String scope) throws Exception {
@@ -37,6 +41,10 @@ public class OclcConnection {
             .apiSecret(clientSecret)
             .defaultScope(scope)
             .build(OclcApi.instance());
+        getToken();
+    }
+
+    private void getToken() {
         try {
             token = oclcService.getAccessTokenClientCredentialsGrant();
         }
@@ -44,11 +52,23 @@ public class OclcConnection {
             log.error("Error connecting to OCLC: ", e);
             return;
         }
+        tokenExpiration = Instant.now().getEpochSecond() + token.getExpiresIn().intValue();
         log.debug("Connected to OCLC");
         log.debug("Response was: " + token.getRawResponse());
     }
 
+    private void checkConnection() {
+        long timeLeft = tokenExpiration - Instant.now().getEpochSecond();
+        log.debug("Token expires in " + timeLeft + " seconds.");
+        if (timeLeft - TOKEN_BUFFER_SECONDS <= 0) {
+            log.debug("Renewing token.");
+            getToken();
+        } 
+    }
+
     public JsonObject execute(String url) throws Exception {
+        checkConnection();
+
         OAuthRequest request = new OAuthRequest(Verb.GET, url);
         request.addHeader("Accept", "application/json");
         oclcService.signRequest(token, request);
