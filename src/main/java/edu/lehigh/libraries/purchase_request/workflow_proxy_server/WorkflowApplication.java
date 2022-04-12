2b143b192e497950c0fa171d1fa1320a610e7ad7
Maccabee Levine <msl321@lehigh.edu>
2022-04-12 15:32:48 -0400

    Package app for .war deployment

--- src/main/java/edu/lehigh/libraries/purchase_request/workflow_proxy_server/WorkflowApplication.java
+++ src/main/java/edu/lehigh/libraries/purchase_request/workflow_proxy_server/WorkflowApplication.java
@@ -3,13 +3,20 @@ package edu.lehigh.libraries.purchase_request.workflow_proxy_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
 @ConditionalOnWebApplication
 @Slf4j
-public class WorkflowApplication {
+public class WorkflowApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WorkflowApplication.class);
+	}
 
 	public static void main(String[] args) {
 		log.info("Starting the Workflow Application");
