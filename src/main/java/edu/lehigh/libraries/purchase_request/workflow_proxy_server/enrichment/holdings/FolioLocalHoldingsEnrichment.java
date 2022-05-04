91c1e9cb22602113a35dd214e0d00c698b327391
Maccabee Levine <msl321@lehigh.edu>
2022-05-04 16:16:59 -0400

    Refactor FolioConnection into separate class

--- src/main/java/edu/lehigh/libraries/purchase_request/workflow_proxy_server/enrichment/holdings/FolioLocalHoldingsEnrichment.java
+++ src/main/java/edu/lehigh/libraries/purchase_request/workflow_proxy_server/enrichment/holdings/FolioLocalHoldingsEnrichment.java
@@ -1,33 +1,14 @@
 package edu.lehigh.libraries.purchase_request.workflow_proxy_server.enrichment.holdings;
 
-import java.net.URI;
-
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-
 import org.apache.commons.lang3.StringUtils;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpHeaders;
-import org.apache.http.HttpVersion;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.CredentialsProvider;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.client.methods.RequestBuilder;
-import org.apache.http.client.utils.URIBuilder;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 
 import edu.lehigh.libraries.purchase_request.model.PurchaseRequest;
 import edu.lehigh.libraries.purchase_request.workflow_proxy_server.Config;
 import edu.lehigh.libraries.purchase_request.workflow_proxy_server.WorkflowService;
+import edu.lehigh.libraries.purchase_request.workflow_proxy_server.connection.FolioConnection;
 import edu.lehigh.libraries.purchase_request.workflow_proxy_server.enrichment.EnrichmentManager;
 import edu.lehigh.libraries.purchase_request.workflow_proxy_server.enrichment.EnrichmentType;
 import lombok.extern.slf4j.Slf4j;
@@ -37,62 +18,18 @@ import lombok.extern.slf4j.Slf4j;
 @ConditionalOnProperty(name="workflow.localHoldings.dataSource", havingValue="FOLIO")
 public class FolioLocalHoldingsEnrichment extends HoldingsEnrichment {
 
-    private static final String LOGIN_PATH = "/authn/login";
     private static final String INSTANCES_PATH = "/inventory/instances";
 
-    private static final String TENANT_HEADER = "x-okapi-tenant";
-    private static final String TOKEN_HEADER = "x-okapi-token";
-
     private final WorkflowService workflowService;
-    private CloseableHttpClient client;
-    private String token;
+    private final FolioConnection connection;
 
     FolioLocalHoldingsEnrichment(EnrichmentManager manager, WorkflowService workflowService, Config config) throws Exception {
         super(config);
         this.workflowService = workflowService;
-        initConnection();
-        initToken();
+        this.connection = new FolioConnection(config);
 
         manager.addListener(this);
         log.debug("FolioLocalHoldingsEnrichment listening.");
-    }
-
-    private void initConnection() {
-        CredentialsProvider provider = new BasicCredentialsProvider();
-        provider.setCredentials(AuthScope.ANY, 
-            new UsernamePasswordCredentials(config.getFolio().getUsername(), config.getFolio().getPassword()));
-        client = HttpClientBuilder.create()
-            .setDefaultCredentialsProvider(provider)
-            .build();                
-    }
-
-    private void initToken() throws Exception {
-        String url = config.getFolio().getOkapiBaseUrl() + LOGIN_PATH;
-        URI uri = new URIBuilder(url).build();
-
-        JsonObject postData = new JsonObject();
-        postData.addProperty("username", config.getFolio().getUsername());
-        postData.addProperty("password", config.getFolio().getPassword());
-        postData.addProperty("tenant", config.getFolio().getTenantId());
-
-        HttpUriRequest post = RequestBuilder.post()
-            .setUri(uri)
-            .setHeader(HttpHeaders.CONTENT_TYPE, ContentType.APPLICATION_JSON.getMimeType())
-            .setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType()).setVersion(HttpVersion.HTTP_1_1)
-            .setHeader(TENANT_HEADER, config.getFolio().getTenantId())
-            .setEntity(new StringEntity(postData.toString()))
-            .build();
-        CloseableHttpResponse response = client.execute(post);
-        HttpEntity entity = response.getEntity();
-        String responseString = EntityUtils.toString(entity);
-        int responseCode = response.getStatusLine().getStatusCode();
-        token = response.getFirstHeader(TOKEN_HEADER).getValue();
-
-        log.debug("got auth response from folio with response code: " + responseCode);
-
-        if (responseCode > 399) {
-            throw new Exception(responseString);
-        }
     }
 
     @Override
@@ -120,7 +57,7 @@ public class FolioLocalHoldingsEnrichment extends HoldingsEnrichment {
             + " and" 
             + " contributors all \"" + contributor + "\""
             + ")";
-        JsonObject responseObject;
+        JSONObject responseObject;
             try {
             responseObject = findMatches(queryString);
         }
@@ -128,7 +65,7 @@ public class FolioLocalHoldingsEnrichment extends HoldingsEnrichment {
             log.error("Exception enriching request: ", e);
             return StringUtils.EMPTY;
         }
-        long totalRecords = responseObject.get("totalRecords").getAsLong();
+        long totalRecords = responseObject.getLong("totalRecords");
 
         return buildEnrichmentMessage(totalRecords, title, contributor, IdentifierType.TitleAndContributor, null, null);
     }
@@ -143,7 +80,7 @@ public class FolioLocalHoldingsEnrichment extends HoldingsEnrichment {
 
         log.debug("Enriching local holdings by " + identifierType + " request for " + purchaseRequest);
         String queryString = "(identifiers =/@value '" + identifier + "')";
-        JsonObject responseObject;
+        JSONObject responseObject;
         try {
             responseObject = findMatches(queryString);
         }
@@ -151,26 +88,13 @@ public class FolioLocalHoldingsEnrichment extends HoldingsEnrichment {
             log.error("Exception enriching request: ", e);
             return StringUtils.EMPTY;
         }
-        long totalRecords = responseObject.get("totalRecords").getAsLong();
+        long totalRecords = responseObject.getLong("totalRecords");
 
         return buildEnrichmentMessage(totalRecords, identifier, null, identifierType, identifierForWebsiteUrl, null);
     }
 
-    private JsonObject findMatches(String queryString) throws Exception {
-        String url = config.getFolio().getOkapiBaseUrl() + INSTANCES_PATH;
-        HttpUriRequest getRequest = RequestBuilder.get()
-            .setUri(url)
-            .setHeader(TENANT_HEADER, config.getFolio().getTenantId())
-            .setHeader(TOKEN_HEADER, token)
-            .addParameter("query", queryString)
-            .build();
-
-        CloseableHttpResponse response = client.execute(getRequest);
-        HttpEntity entity = response.getEntity();
-        String responseString = EntityUtils.toString(entity);
-        log.debug("Got response with code " + response.getStatusLine() + " and entity " + response.getEntity());
-
-        JsonObject responseObject = JsonParser.parseString(responseString).getAsJsonObject();
+    private JSONObject findMatches(String queryString) throws Exception {
+        JSONObject responseObject = connection.executeGet(INSTANCES_PATH, queryString);
         return responseObject;
     }
 
@@ -190,6 +114,4 @@ public class FolioLocalHoldingsEnrichment extends HoldingsEnrichment {
         return message;
     }
 
-
 }
-
