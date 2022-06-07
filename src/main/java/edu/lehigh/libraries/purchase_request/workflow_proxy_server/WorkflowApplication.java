bff974e16ed23846a9551f9b7cc4b9e6a6623cc4
Maccabee Levine <msl321@lehigh.edu>
2022-06-07 11:19:45 -0400

    Report enrichment and listener services (and counts) on startup

--- src/main/java/edu/lehigh/libraries/purchase_request/workflow_proxy_server/WorkflowApplication.java
+++ src/main/java/edu/lehigh/libraries/purchase_request/workflow_proxy_server/WorkflowApplication.java
@@ -1,14 +1,24 @@
 package edu.lehigh.libraries.purchase_request.workflow_proxy_server;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FullyQualifiedAnnotationBeanNameGenerator;
 
+import edu.lehigh.libraries.purchase_request.workflow_proxy_server.enrichment.EnrichmentManager;
+import edu.lehigh.libraries.purchase_request.workflow_proxy_server.listeners.WorkflowServiceListener;
 import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@ComponentScan(nameGenerator = FullyQualifiedAnnotationBeanNameGenerator.class)
 @ConditionalOnWebApplication
 @Slf4j
 public class WorkflowApplication extends SpringBootServletInitializer {
@@ -18,10 +28,29 @@ public class WorkflowApplication extends SpringBootServletInitializer {
 		return application.sources(WorkflowApplication.class);
 	}
 
+	private static void reportServices(ApplicationContext context) {
+		String[] beanNames = context.getBeanDefinitionNames();
+		reportServices(beanNames, EnrichmentManager.class);
+		reportServices(beanNames, WorkflowServiceListener.class);
+	}
+
+	private static void reportServices(String[] beanNames, Class<?> clazz) {
+		List<String> services = Stream.of(beanNames)
+			.filter(name -> name.contains(clazz.getPackageName()))
+			.filter(name -> !name.equals(clazz.getCanonicalName()))
+			.map(name -> name.substring(clazz.getPackageName().length() + 1))
+			.collect(Collectors.toList());
+		log.info(services.size() + " " + clazz.getSimpleName() + " services loaded:");
+		for (String name: services) {
+			log.info("... " + name);
+		}
+	}
+
 	public static void main(String[] args) {
 		log.info("Starting the Workflow Application");
-		SpringApplication.run(WorkflowApplication.class, args);
-        log.info("Workflow Application finished");
+		ApplicationContext context = SpringApplication.run(WorkflowApplication.class, args);
+		reportServices(context);
+        log.info("Workflow Application started");
 	}
 
 }
